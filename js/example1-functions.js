0025e0d1b761f895b7c0a02a3cc07458ed3a53ea
a-rede <abell.rede1@outlook.com>
2022-11-15 11:03:17 -0800

    Image update

--- js/example1-functions.js
+++ js/example1-functions.js
@@ -18,4 +18,67 @@
 
 // greet("Abell");
 
+// Example 2: 
+// Old function declaration
+// function greet(name, day = "Day") {
+//     console.log(`Good ${day} ${name}!`);
+// };
+
+// // Anonymous function declaration
+// let greet = function(name, day = "Day") {
+//     console.log(`Good ${day} ${name}!`);
+// };
+
+// // Arrowhead function declaration
+// let greet = (name, day = "Day") => {
+//     console.log(`Good ${day} ${name}!`);
+// };
+
+// greet("Abell");
+// greet("Abell", "Night");
+
+// Example 3: Circle Radius
+// let input = prompt("Enter a value: ")
+
+// let circleArea = input => {
+
+// } 
+
+// Example 4: Add DOM elements
+const unorderedListElement = document.querySelector(".shopping"); // DOM ELement
+
+function populateList(list) {
+    for(let i = 0; i < list.length; i++){
+        // Creates li element
+        const listItemElement = document.createElement("li");
+        
+        // adds the list content to the ul 
+        listItemElement.textContent = list[i];
+
+        // appends the the listItemElement to the UL
+        unorderedListElement.appendChild(listItemElement);
+    }
+};
+
+let shoppingList = ['cheese', 'bread', 'green pepper'];
+
+populateList(shoppingList);
+
+// Example 5: Modifying CSS with JavaScript
+
+function listStyleChange() {
+    unorderedListElement.classList.replace("circleList", "squareList");
+}
+
+listStyleChange();
+
+function updateImage() {
+    const imageElement = document.querySelector("#shoppingCart");
+    imageElement.src = "https://cdn-icons-png.flaticon.com/512/263/263142.png";
+    imageElement.width = 100;
+    imageElement.height = 100;
+};
+
+updateImage();
+
 
